90380ceb45872a883ad66f945334de359ea4bb00
Ilya Smolkov <ilia.smolkov@telekom.com>
2022-02-16 16:19:03 +0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = local.region 
+}
+
+locals {
+  name            = "eks-${var.env_prefix}"
+  cluster_version = "1.21"
+  region          = "eu-central-1"
+}
+
+data "aws_availability_zones" "available_azs" {}
+
+# output "test" {
+#   value = data.aws_availability_zones.available_azs.zone_ids[0]
+# }
+
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+  cidr = "10.0.0.0/16"
+
+  azs             = [data.aws_availability_zones.available_azs.zone_ids[0]]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+ 
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/elb"              = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/internal-elb"     = 1
+  }
+}
+
+resource "aws_security_group" "additional" {
+  name_prefix = "${var.env_prefix}-additional-sg"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
+
